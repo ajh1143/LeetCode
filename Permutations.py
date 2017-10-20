3e3d18e97e9d246cf294b1fd5282d2374afbe39b
Andrew Holt <15341747+ajh1143@users.noreply.github.com>
2017-10-20 12:07:09 -0700

    Create Permutations.py

---
+++ Permutations.py
@@ -0,0 +1,23 @@
+#46. Permutations
+#Prompt:
+#Given a collection of distinct numbers, return all possible permutations.
+
+#For example,
+#[1,2,3] have the following permutations:
+#[
+#  [1,2,3],
+#  [1,3,2],
+#  [2,1,3],
+#  [2,3,1],
+#  [3,1,2],
+#  [3,2,1]
+#]
+
+#SOLUTION:
+
+import itertools
+class Solution(object):
+    def permute(self, nums):
+       count = len(nums)
+       x = (list(itertools.permutations(nums, count)))
+       return x
